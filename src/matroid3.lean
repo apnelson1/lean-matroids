01db7e620cd38d517d1a756c8be0d7408f8c88fe
Edward Lee <e45lee@uwaterloo.ca>
2020-10-15 15:29:36 -0400

    Change matroids to use size instead of card.

--- src/matroid3.lean
+++ src/matroid3.lean
@@ -2,6 +2,7 @@ import data.fintype.basic
 import data.set 
 import data.finset
 import tactic
+import size 
 
 noncomputable theory
 localized "attribute [instance, priority 100000] classical.prop_decidable
@@ -13,10 +14,23 @@ universes u v
 
 @[ext]
 structure matroid (γ : Type u) [fintype γ] :=
-(r : finset γ → ℕ)
-(R1 : ∀ (X : finset γ), r X ≤ X.card)
+(r : finset γ → ℤ)
+(R0 : ∀ (X : finset γ ), 0 ≤ r X)
+(R1 : ∀ (X : finset γ), r X ≤ size X)
 (R2 : ∀ {X Y : finset γ}, X ⊆ Y → r X ≤ r Y)
 (R3 : ∀ (X Y : finset γ), r (X ∪ Y) + r (X ∩ Y) ≤ r X + r Y)
+
+/-
+type mismatch at application
+  size X
+term
+  X
+has type
+  finset γ : Type u
+but is expected to have type
+  finset ?m_1 : Type
+All Messages (115)
+-/
 
 namespace matroid
 variables {γ : Type u} [fintype γ] (M : matroid γ)
@@ -24,8 +38,8 @@ variables {γ : Type u} [fintype γ] (M : matroid γ)
 abbreviation E (M : matroid γ) : finset γ := univ
 
 @[simp]
-lemma r_empty_eq_zero : M.r ∅ = 0 := 
-by linarith [M.R1 ∅, @card_empty γ]
+lemma r_empty_eq_zero : M.r ∅ = 0 :=  
+by linarith [(M.R0 ∅), (M.R1 ∅), @size_empty γ]
 
 @[simp]
 lemma r_univ_compl_eq_zero : M.r M.Eᶜ = 0 :=
@@ -35,7 +49,8 @@ by { convert M.r_empty_eq_zero, convert_to ⊤ᶜ = _, simp only [compl_top], re
 subset_univ X
 
 lemma r_subadditive (X Y : finset γ) : M.r (X ∪ Y) ≤ M.r X + M.r Y :=
-le_trans (nat.le.intro rfl) (M.R3 X Y)
+by
+  linarith [(M.R0 (X ∩ Y)), (M.R1 (X ∩ Y)), (M.R3 X Y)]
 
 lemma r_le_union (X Y : finset γ) : M.r X ≤ M.r (X ∪ Y) :=
 by { apply R2, apply subset_union_left }
@@ -53,7 +68,7 @@ begin
   simp [compl_eq_univ_sdiff, subset_univ],
 end
 
-lemma corank_le (M : matroid γ) (X : finset γ) : M.r M.E - M.r Xᶜ ≤ X.card :=
+lemma corank_le (M : matroid γ) (X : finset γ) : M.r M.E - M.r Xᶜ ≤ size X :=
 begin
   have hxc := M.R2 (M.subset_E Xᶜ),
   have h1 := M.r_compl X,
@@ -65,24 +80,31 @@ end
 The dual matroid
 -/
 def dual (M : matroid γ) : matroid γ :=
-{ r := λ X, X.card + M.r Xᶜ - M.r M.E,
-  R1 := λ X, begin
-    have h : M.r Xᶜ ≤ M.r M.E := M.R2 (subset_univ _),
-    omega,
+{ r := λ X, size X + M.r Xᶜ - M.r M.E,
+  R0 := λ X, begin
+    linarith [r_compl M X, M.R1 X],
   end,
+  R1 := λ X, 
+  begin
+    have h : M.r Xᶜ ≤ M.r M.E := M.R2 (subset_univ _),
+    linarith,
+  end,
+  
   R2 := λ X Y hXY, begin 
-    sorry,
+    -- want to show: |X| + r(E \ X) < |Y| + r(E \ Y)
+    have YsetminusX : size (Y \ X) = size Y - size X,
+    { exact size_sdiff hXY },
+    have YC : Yᶜ ∪ (Y \ X) = Xᶜ ,
+    { sorry },
+    have rYC : M.r(Yᶜ ∪ (Y \ X)) = M.r(Xᶜ),
+    { rw YC },
+    linarith [(r_subadditive M Yᶜ (Y \ X)), M.R1 (Y \ X)],
   end,
   R3 := λ X Y, begin
-    sorry
+    -- linarith [M.R3 Xᶜ Yᶜ, size_modular X Y],
+    sorry,
   end }
 
-/-
-invalid field notation, type is not of the form (C ...) where C is a constant
-  M'
-has type
-  ⁇
--/
 end matroid 
 /--
 A "submatroid" is the matroid restricted to a subset then contracted.
@@ -98,9 +120,14 @@ The rank function is then meant to be for subsets of `E` that contain
 
 namespace minor 
 variables {γ : Type u} [fintype γ] {M : matroid γ}
-def r (M' : minor M) (X : finset γ) : ℕ := M.r (X ∪ M'.C) - M.r M'.C --This allows rank-evaluations of sets intersecting C ∪ D - maybe this is ok. 
+def r (M' : minor M) (X : finset γ) : ℤ := M.r (X ∪ M'.C) - M.r M'.C --This allows rank-evaluations of sets intersecting C ∪ D - maybe this is ok. 
 
-lemma R1 (M' : minor M) (X : finset γ) : M'.r X ≤ X.card :=
+lemma R0 (M' : minor M) (X : finset γ) : 0 ≤ M'.r X :=
+begin
+  sorry
+end
+
+lemma R1 (M' : minor M) (X : finset γ) : M'.r X ≤ size X :=
 begin
   sorry
 end
@@ -118,7 +145,8 @@ end
 
 def to_matroid (M' : minor M) : matroid (↑(M.E \ M'.C \ M'.D) : set γ) :=
 { r := λ X, M'.r (X.map (function.embedding.subtype _)),
-  R1 := λ X, by { rw ← card_map, apply M'.R1 },
+  R0 := λ X, M'.R0 (X.map (function.embedding.subtype _)),
+  R1 := λ X, by { rw ← size_map, apply M'.R1 },
   R2 := λ X Y hs, M'.R2 (map_subset_map.mpr hs),
   R3 := λ X Y, by { rw [map_union, map_inter], apply M'.R3 } }
 @[simp]
@@ -157,9 +185,9 @@ namespace submatroid
 variables {γ : Type u} [fintype γ] {M : matroid γ}
 
 
-def r (M' : submatroid M) (X : finset γ) : ℕ := M.r (X ∪ M'.F) - M.r M'.F
+def r (M' : submatroid M) (X : finset γ) : ℤ := M.r (X ∪ M'.F) - M.r M'.F
 
-lemma R1 (M' : submatroid M) (X : finset γ) : M'.r X ≤ X.card :=
+lemma R1 (M' : submatroid M) (X : finset γ) : M'.r X ≤ size X :=
 begin
   dunfold r,
   have h1 := M.R1 X,
@@ -198,7 +226,8 @@ end
 
 def to_matroid (M' : submatroid M) : matroid (↑M'.E : set γ) :=
 { r := λ X, M'.r (X.map (function.embedding.subtype _)),
-  R1 := λ X, by { rw ← card_map, apply M'.R1 },
+  R0 := sorry,
+  R1 := λ X, by { rw ← size_map, apply M'.R1 },
   R2 := λ X Y hs, M'.R2 (map_subset_map.mpr hs),
   R3 := λ X Y, by { rw [map_union, map_inter], apply M'.R3 } }
 
@@ -301,18 +330,18 @@ def contract (M : matroid γ) (F : finset γ) : submatroid M :=
 
 
 @[simp] lemma dual_E_eq (M : matroid γ) : M.dual.E = M.E := rfl
-@[simp] lemma dual_r_eq (M : matroid γ) (X : finset γ) : M.dual.r X = X.card + M.r Xᶜ - M.r M.E := rfl
+@[simp] lemma dual_r_eq (M : matroid γ) (X : finset γ) : M.dual.r X = size X + M.r Xᶜ - M.r M.E := rfl
 
 lemma dual_dual_eq (M : matroid γ) : M.dual.dual = M :=
 begin
   ext X,
-  change X.card + M.dual.r Xᶜ - M.dual.r M.dual.E = M.r X,
-  rw dual_E_eq, rw [dual_r_eq, dual_r_eq], simp [finset.card_compl],
-  have hh : fintype.card γ = M.E.card := rfl, rw hh,
+  change size X + M.dual.r Xᶜ - M.dual.r M.dual.E = M.r X,
+  rw dual_E_eq, rw [dual_r_eq, dual_r_eq], simp [size_compl],
+  have hh : type_size γ  = size M.E := begin sorry end,
   have h1 := M.R1 X, have h2 := M.R1 M.E,
   have h3 := M.subset_E X, have h4 := M.R2 h3,
-  have h5 := card_le_of_subset h3,
-  sorry, -- just need a few more inequalities to make omega happy, I think.
+  have h5 := size_le_of_subset h3,
+  linarith,
 end
 
 @[simp]
@@ -339,7 +368,7 @@ end
 lemma mdelete_D (M : matroid γ) (D : finset γ) : (M.mdelete D).D = D := rfl
 
 lemma clear_emptl {C D : finset γ} (h : C ∩ D = ∅) : C ∩ (∅ ∪ D) = ∅ := by tidy
-lemma clear_emptr {C D : finset γ} (h : C ∩ D = ∅) : D ∩ (C ∪ ∅)  = ∅ := sorry 
+lemma clear_emptr {C D : finset γ} (h : C ∩ D = ∅) : D ∩ (C ∪ ∅) = ∅ := sorry 
 
 
 lemma cd_eq_dc (M: matroid γ) (C: finset γ) (D: finset γ) (h: C ∩ D = ∅) :
