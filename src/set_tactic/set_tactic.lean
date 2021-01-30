02cb7b62056f506ff90c5a5884c3c8b03f52afdb
Edward Lee <e45lee@uwaterloo.ca>
2021-01-29 21:04:59 -0500

    Clean up instances of univ and empty.

--- src/set_tactic/set_tactic.lean
+++ src/set_tactic/set_tactic.lean
@@ -32,9 +32,10 @@ lemma set_union_sup (T : Type) (A B : set T) : (A ∪ B) = (A ⊔ B) := by refl
 lemma set_inter_inf (T : Type) (A B : set T) : (A ∩ B) = (A ⊓ B) := by refl
 lemma set_subset_le (T : Type) (A B : set T) : (A ⊆ B) = (A ≤ B) := by refl
 lemma set_subset_lt (T : Type) (A B : set T) : (A ⊂ B) = (A < B) := by refl
-
+lemma set_univ_top (T : Type) : (set.univ : set T) = ⊤ := by refl
+lemma set_empt_bot (T : Type) : (∅ : set T) = ⊥ := by refl 
 
 meta def set_cleanup : tactic unit :=
   `[simp only [cleanup.set_union_sup, cleanup.set_inter_inf, cleanup.set_subset_le,
-      cleanup.set_subset_lt] at *]
+      cleanup.set_subset_lt, cleanup.set_univ_top, cleanup.set_empt_bot] at *]
 end cleanup
