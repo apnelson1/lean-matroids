e9ad7fa65fbe1312ae7a8d7d5aa0a9aac0505f32
Alena Gusakov <agusakov@udel.edu>
2023-03-30 17:27:53 -0400

    finished of_base

--- src/matroid/representation/lin_rep.lean
+++ src/matroid/representation/lin_rep.lean
@@ -19,7 +19,24 @@ def is_representable (M : matroid E) (𝔽 : Type*) [field 𝔽] : Prop :=
 lemma of_base (φ : rep 𝔽 M ι) {B : set E} (hB : M.base B) (e : E) : 
   φ.to_fun e ∈ submodule.span 𝔽 (φ.to_fun '' B) := 
 begin
-  
+  by_cases e ∈ B,
+  { have h2 := @submodule.subset_span 𝔽 _ _ _ _ (φ.to_fun '' B),
+    have h3 : φ.to_fun e ∈ (φ.to_fun '' B),
+    apply (set.mem_image φ.to_fun B (φ.to_fun e)).2,
+    use e,
+    use h,
+    have h4 := set.mem_of_subset_of_mem h2 h3,
+    simp at h4,
+    exact h4 },
+  have h2 : ¬ linear_independent 𝔽 (λ f : insert e B, φ.to_fun (f : E)),
+  { rw rep.valid,
+    apply base.dep_of_insert hB h },
+  by_contra h3,
+  apply h2,
+  rw linear_independent_insert' h,
+  refine ⟨_, h3⟩,
+  rw rep.valid,
+  apply base.indep hB,
 end 
 
 
