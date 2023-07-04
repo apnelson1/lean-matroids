a8749485591a2e7bd4d7785df9a4c8d4c0802504
Tom Iagovet <artemie.iagovet@gmail.com>
2023-07-04 17:33:58 -0400

    wip

--- src/m_in/connectivity.lean
+++ src/m_in/connectivity.lean
@@ -298,38 +298,56 @@ def direct_Sum' {ι : Type*} (Ms : ι → matroid_in α)
         rw h', exact h₂I j, } },
   end)
   (begin -- a maximal indep. set exists
-    rintro X hX I ⟨Is, hIs, rfl⟩ hIsX,
+    rintro X hX I ⟨h₁I, h₂I⟩ hIX,
     let Xs := λ i, X ∩ (Ms i).E,
-    have hIsXs : ∀ i, (Is i) ⊆ (Xs i) :=
-      λ i e he, ⟨hIsX ((subset_Union Is i) he), (hIs i).subset_ground he⟩, 
-    have h : ∀ i, ∃ B, (Ms i).basis B (Xs i) ∧ (Is i) ⊆ B :=
-      λ i, (hIs i).subset_basis_of_subset (hIsXs i),
-    choose! Bs hBs using h,
-    refine ⟨Union Bs, ⟨_, _⟩⟩,
-    { simp only [Union_subset_iff, mem_set_of_eq],
-      refine ⟨⟨Bs, ⟨λ i, (hBs i).1.indep, by { refl }⟩⟩,
-        ⟨λ i, (hBs i).2.trans (subset_Union Bs i),
-        λ i, (hBs i).1.subset.trans (inter_subset_left X (Ms i).E),⟩⟩, },
-    { simp only [Union_subset_iff, mem_set_of_eq, and_imp, forall_exists_index],
-      rintro J Js hJs rfl hIsJ hJX hBsJ,
-      simp only [Union_subset_iff],
-      have hBsJs : ∀ i, (Bs i) ⊆ (Js i),
-        -- question: how could this proof be made shorter? 
-        { rintro i e he, have := (hBsJ i) he,
-          simp only [mem_Union] at this,
-          obtain ⟨j, hj⟩ := this,
-          by_cases g : i = j,
-          { rw g, exact hj },
-          { exfalso,
-            have : e ∈ (Ms i).E ∩ (Ms j).E :=
-              ⟨(hBs i).1.subset_ground_left he, (hJs j).subset_ground hj⟩,
-            rw h i j g at this, exact not_mem_empty e this } },
-        -- ends here
-      have hJsXs : ∀ i, (Js i) ⊆ (Xs i) :=
-        λ i e he, ⟨hJX (subset_Union Js i he), (hJs i).subset_ground he⟩,
-      rintro i,
-      rw ←(hBs i).1.eq_of_subset_indep (hJs i) (hBsJs i) (hJsXs i),
-      exact subset_Union Bs i, }
+    have h' : ∀ i, ∃ B, (Ms i).basis B (Xs i) :=
+        λ i, (Ms i).exists_basis (Xs i),
+    choose! Bs hBs using h',
+
+    
+    refine ⟨Union Bs, ⟨⟨⟨λ e he, _, λ i, _⟩,_⟩, _⟩⟩,
+    { rw mem_Union at he |-,
+      obtain ⟨i, hi⟩ := he,
+      exact ⟨i, (hBs i).subset_ground_left hi⟩ },
+    { have h' : ∀ i, Union Bs ∩ (Ms i).E = (Bs i) := sorry,
+      simp_rw h', exact (hBs i).indep },
+    {
+      
+    }
+
+
+    -- rintro X hX I ⟨Is, hIs, rfl⟩ hIsX,
+    -- let Xs := λ i, X ∩ (Ms i).E,
+    -- have hIsXs : ∀ i, (Is i) ⊆ (Xs i) :=
+    --   λ i e he, ⟨hIsX ((subset_Union Is i) he), (hIs i).subset_ground he⟩, 
+    -- have h : ∀ i, ∃ B, (Ms i).basis B (Xs i) ∧ (Is i) ⊆ B :=
+    --   λ i, (hIs i).subset_basis_of_subset (hIsXs i),
+    -- choose! Bs hBs using h,
+    -- refine ⟨Union Bs, ⟨_, _⟩⟩,
+    -- { simp only [Union_subset_iff, mem_set_of_eq],
+    --   refine ⟨⟨Bs, ⟨λ i, (hBs i).1.indep, by { refl }⟩⟩,
+    --     ⟨λ i, (hBs i).2.trans (subset_Union Bs i),
+    --     λ i, (hBs i).1.subset.trans (inter_subset_left X (Ms i).E),⟩⟩, },
+    -- { simp only [Union_subset_iff, mem_set_of_eq, and_imp, forall_exists_index],
+    --   rintro J Js hJs rfl hIsJ hJX hBsJ,
+    --   simp only [Union_subset_iff],
+    --   have hBsJs : ∀ i, (Bs i) ⊆ (Js i),
+    --     -- question: how could this proof be made shorter? 
+    --     { rintro i e he, have := (hBsJ i) he,
+    --       simp only [mem_Union] at this,
+    --       obtain ⟨j, hj⟩ := this,
+    --       by_cases g : i = j,
+    --       { rw g, exact hj },
+    --       { exfalso,
+    --         have : e ∈ (Ms i).E ∩ (Ms j).E :=
+    --           ⟨(hBs i).1.subset_ground_left he, (hJs j).subset_ground hj⟩,
+    --         rw h i j g at this, exact not_mem_empty e this } },
+    --     -- ends here
+    --   have hJsXs : ∀ i, (Js i) ⊆ (Xs i) :=
+    --     λ i e he, ⟨hJX (subset_Union Js i he), (hJs i).subset_ground he⟩,
+    --   rintro i,
+    --   rw ←(hBs i).1.eq_of_subset_indep (hJs i) (hBsJs i) (hJsXs i),
+    --   exact subset_Union Bs i, }
   end)
   (begin -- indep sets contained in ground set
     rintro I ⟨I', I'ind, rfl⟩,
